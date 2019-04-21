df60632498073857b07f1bf29615e972663ab580
breadbread1984 <breadbread1984@163.com>
2019-04-21 20:46:46 +0800

    serving code

--- start_serving.sh
+++ start_serving.sh
@@ -6,3 +6,7 @@ curl https://storage.googleapis.com/tensorflow-serving-apt/tensorflow-serving.re
 sudo apt update
 sudo apt install tensorflow-model-server
 
+python3 convert_model.py
+saved_model_cli show --dir ./serving_model --all # checkout output model
+saved_model_cli run --dir ./serving_model --tag_set serve --signature_def serving_default --input_exp 'input_image=np.random.normal(size=(1,28,28,1))' # test serving
+
