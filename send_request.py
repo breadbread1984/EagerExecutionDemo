70c3f6fc41c9f481f942af8c674079c6848edccc
Konquer your Mind <breadbread1984@163.com>
2019-05-12 09:31:31 +0800

    Update send_request.py

--- send_request.py
+++ send_request.py
@@ -5,7 +5,7 @@ import requests;
 import numpy as np;
 
 x = np.random.normal(size=(1,28,28,1));
-data = json.dumps({"signature_name": "serving_default", "instances": x.tolist()});
+data = json.dumps({"signature_name": "serving_default", "instances": [{"input_1": x.tolist()}]});
 headers = {"content-type": "application/json"};
 json_response = requests.post('http://localhost:8501/v1/models/mnist:predict', data = data, headers = headers);
 predictions = np.array(json.loads(json_response.text)["predictions"]);
