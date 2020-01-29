fc2946b6f72fc21d81711fb290da4f4c664d0d23
breadbread1984 <breadbread1984@163.com>
2020-01-29 18:54:22 +0800

    update train

--- train_mnist.py
+++ train_mnist.py
@@ -24,12 +24,12 @@ def parse_function(serialized_example):
 def main():
     #create model and optimizer
     model = MNISTModel();
-    optimizer = tf.keras.optimizers.Adam(tf.keras.optimizers.schedules.ExponentialDecay(1e-3, decay_steps = 60000, decay_rate = 0.9));
+    optimizer = tf.keras.optimizers.Adam(tf.keras.optimizers.schedules.ExponentialDecay(1e-3, decay_steps = 60000, decay_rate = 0.5));
     #load dataset
-    trainset = iter(tf.data.TFRecordDataset(os.path.join('dataset','trainset.tfrecord')).map(parse_function).shuffle(batch_size).batch(batch_size).prefetch(tf.data.experimental.AUTOTUNE));
+    trainset = iter(tf.data.TFRecordDataset(os.path.join('dataset','trainset.tfrecord')).repeat(-1).map(parse_function).shuffle(batch_size).batch(batch_size).prefetch(tf.data.experimental.AUTOTUNE));
     #restore from existing checkpoint
     if False == os.path.exists('checkpoints'): os.mkdir('checkpoints');
-    checkpoint = tf.train.Checkpoint(model = model,optimizer = optimizer, optimizer_step = optimizer.iterations);
+    checkpoint = tf.train.Checkpoint(model = model, optimizer = optimizer);
     checkpoint.restore(tf.train.latest_checkpoint('checkpoints'));
     #create log
     log = tf.summary.create_file_writer('checkpoints');
@@ -47,14 +47,14 @@ def main():
         if tf.equal(optimizer.iterations % 100, 0):
             with log.as_default():
                 tf.summary.scalar('loss',avg_loss.result(), step = optimizer.iterations);
-            print('Step #%d Loss: %.6f' % (optimizer.iterations,avg_loss.result()));
+            print('Step #%d Loss: %.6f lr: %.6f' % (optimizer.iterations,avg_loss.result(), optimizer._hyper['learning_rate'](optimizer.iterations)));
+            if avg_loss.result() < 0.01: break;
             avg_loss.reset_states();
         grads = tape.gradient(loss,model.trainable_variables);
         optimizer.apply_gradients(zip(grads,model.trainable_variables));
         #save model
         if tf.equal(optimizer.iterations % 100, 0):
             checkpoint.save(os.path.join('checkpoints','ckpt'));
-        if loss < 0.01: break;
     #save the network structure with weights
     if False == os.path.exists('model'): os.mkdir('model');
     model.save('./model/mnist_model.h5');
